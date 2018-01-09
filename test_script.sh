aba132a9173319695b10c7b6b6ba7084fab0f3ce
Madelyn Petersohn <maddie.petersohn@gmail.com>
2018-01-09 08:48:06 -0800

    Rebased

--- test_script.sh
+++ test_script.sh
@@ -5,8 +5,8 @@ flake8 .
 
 # run pytest for python3 on all test files
 python3 -m pytest test/local_manager_test.py
-# python3 -m pytest test/graph_test.py
+python3 -m pytest test/graph_test.py
 
 #run pytest for python2 on all test files
 python2 -m pytest test/local_manager_test.py
-# python2 -m pytest test/graph_test.py
+python2 -m pytest test/graph_test.py
