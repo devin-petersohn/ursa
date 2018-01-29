28243bbac110f10fd6970e77d04235705a935986
Kunal Gosar <gosarkunal9@gmail.com>
2018-01-29 14:01:44 -0800

    basic test and fix for write rows

--- test/serialization_test.py
+++ test/serialization_test.py
@@ -5,4 +5,14 @@ ray.init()
 
 
 def test_write_rows():
-    pass
+    test_data = "AGCGCTGTAGGGACACTGCAGGGAGGCCTCTGCTGCCCTGCT"
+    e1 = ursa.graph.Edge("CTGCAGGGAG", 1, "1")
+    e2 = ursa.graph.Edge("TGCAGGGAG", 2, "2")
+    e3 = ursa.graph.Edge("CTCTGCT", 3, "3")
+
+    test_row = ursa.graph.graph._GraphRow(test_data,
+                                    set([e1, e2]),
+                                    {"graph2": set([e2, e3])}, 3)
+
+    dest = ursa.graph.utils.write_row(test_row, "graph1", "node1")
+    print(dest)
