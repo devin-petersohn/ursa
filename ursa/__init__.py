cb4eed15a1130a8d82d547b742699ba3dae4b0e4
Adithya Girish <adithyagirish@berkeley.edu>
2018-02-12 13:55:09 -0800

    Updating IO API to be more canonical

--- ursa/__init__.py
+++ ursa/__init__.py
@@ -3,6 +3,6 @@ from .local_manager import GraphManager
 from . import graph
 from .io.reader import read_csv
 
-__all__ = ["database", "GraphManager", "graph", "io"]
+__all__ = ["database", "GraphManager", "graph", "io", "read_csv"]
 
 __version__ = "0.0.1-SNAPSHOT"
--- ursa/io/__init__.py
+++ ursa/io/__init__.py
@@ -1,9 +1,5 @@
-<<<<<<< 89b20321eae1a147455ad676f9d74afef5dff4a7
-__all__ = []
-=======
 from .reader import reader
 
 __all__ = ["reader"]
->>>>>>> resolving rebase issues
 
 __version__ = "0.0.1-SNAPSHOT"
