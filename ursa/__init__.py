effb1c9f712955a8b0d0410d51a1c89e8364f95f
Adithya Girish <adithyagirish@berkeley.edu>
2018-02-12 17:01:11 -0800

    resolving rebase issues

--- ursa/__init__.py
+++ ursa/__init__.py
@@ -3,6 +3,6 @@ from .local_manager import GraphManager
 from . import graph
 from .io.reader import read_csv
 
-__all__ = ["database", "Graph_manager", "graph", "io"]
+__all__ = ["database", "GraphManager", "graph", "io"]
 
 __version__ = "0.0.1-SNAPSHOT"
